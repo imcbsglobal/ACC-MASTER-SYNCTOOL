27bb7d2e2dd5a529d07b6f0044ff78dc38319108
Krishna Kumar <149694348+KRISHNAKUMARPS2002@users.noreply.github.com>
2025-07-16 09:39:41 +0530

    Table mapping edited to fetch acc_ledgers

--- table_mapping.py
+++ table_mapping.py
@@ -1,21 +1,5 @@
 TABLE_MAPPING = {
-    "IMC": {
-        "query": """
-            SELECT 
-                acc_master.code,
-                acc_master.name,
-                acc_master.opening_balance,
-                acc_master.debit,
-                acc_master.credit,
-                acc_master.place,
-                acc_master.phone2,
-                acc_departments.department AS openingdepartment
-            FROM acc_master
-            LEFT JOIN acc_departments ON acc_master.openingdepartment = acc_departments.department_id
-            WHERE acc_master.super_code IN ('IMCC', 'IMCDI', 'IMK', 'IMM', 'RC', 'SYSK')
-        """
-    },
-    "IMCB": {
+    "SYSMAC": {
         "query": """
             SELECT 
                 acc_master.code,
@@ -29,6 +13,86 @@ TABLE_MAPPING = {
             FROM acc_master
             LEFT JOIN acc_departments ON acc_master.openingdepartment = acc_departments.department_id
             WHERE acc_master.super_code = 'DEBTO'
+        """
+    },
+    "SYSMAC LEDGERS": {
+        "query": """
+            SELECT
+                acc_ledgers.code,
+                acc_ledgers.particulars,
+                acc_ledgers.debit,
+                acc_ledgers.credit,
+                acc_ledgers.entry_mode,
+                acc_ledgers."date" AS entry_date,
+                CAST(acc_ledgers.voucher_no AS INTEGER) AS voucher_no,
+                acc_ledgers.narration
+            FROM acc_ledgers
+            LEFT JOIN acc_master ON acc_ledgers.code = acc_master.code
+            WHERE acc_master.super_code = 'DEBTO';
+        """
+    },
+    "IMCB LLP": {
+        "query": """
+            SELECT 
+                acc_master.code,
+                acc_master.name,
+                acc_master.opening_balance,
+                acc_master.debit,
+                acc_master.credit,
+                acc_master.place,
+                acc_master.phone2,
+                acc_departments.department AS openingdepartment
+            FROM acc_master
+            LEFT JOIN acc_departments ON acc_master.openingdepartment = acc_departments.department_id
+            WHERE acc_master.super_code = 'DEBTO'
+        """
+    },
+    "IMC LLP LEDGERS": {
+        "query": """
+            SELECT
+                acc_ledgers.code,
+                acc_ledgers.particulars,
+                acc_ledgers.debit,
+                acc_ledgers.credit,
+                acc_ledgers.entry_mode,
+                acc_ledgers."date" AS entry_date,
+                CAST(acc_ledgers.voucher_no AS INTEGER) AS voucher_no,
+                acc_ledgers.narration
+            FROM acc_ledgers
+            LEFT JOIN acc_master ON acc_ledgers.code = acc_master.code
+            WHERE acc_master.super_code = 'DEBTO';
+        """
+    },
+    "IMC": {
+        "query": """
+            SELECT 
+                acc_master.code,
+                acc_master.name,
+                acc_master.opening_balance,
+                acc_master.debit,
+                acc_master.credit,
+                acc_master.place,
+                acc_master.phone2,
+                acc_departments.department AS openingdepartment
+            FROM acc_master
+            LEFT JOIN acc_departments ON acc_master.openingdepartment = acc_departments.department_id
+            WHERE acc_master.super_code = 'DEBTO'
+        """
+    },
+    "IMC LEDGERS": {
+        "query": """
+            SELECT
+                acc_ledgers.code,
+                acc_ledgers.particulars,
+                acc_ledgers.debit,
+                acc_ledgers.credit,
+                acc_ledgers.entry_mode,
+                acc_ledgers."date" AS entry_date,
+                CAST(acc_ledgers.voucher_no AS INTEGER) AS voucher_no,
+                acc_ledgers.narration
+            FROM acc_ledgers
+            LEFT JOIN acc_master ON acc_ledgers.code = acc_master.code
+            WHERE acc_master.super_code = 'DEBTO';
         """
     },
     "Sysmac_INFO": {
@@ -47,6 +111,22 @@ TABLE_MAPPING = {
             WHERE acc_master.super_code = 'DEBTO'
         """
     },
+    "Sysmac_INFO LEDGERS": {
+        "query": """
+            SELECT
+                acc_ledgers.code,
+                acc_ledgers.particulars,
+                acc_ledgers.debit,
+                acc_ledgers.credit,
+                acc_ledgers.entry_mode,
+                acc_ledgers."date" AS entry_date,
+                CAST(acc_ledgers.voucher_no AS INTEGER) AS voucher_no,
+                acc_ledgers.narration
+            FROM acc_ledgers
+            LEFT JOIN acc_master ON acc_ledgers.code = acc_master.code
+            WHERE acc_master.super_code = 'DEBTO';
+        """
+    },
     "DQ": {
         "query": """
             SELECT 
@@ -63,20 +143,20 @@ TABLE_MAPPING = {
             WHERE acc_master.super_code = 'DEBTO'
         """
     },
-    "SYSMAC": {
+    "DQ LEDGERS": {
         "query": """
-            SELECT 
-                acc_master.code,
-                acc_master.name,
-                acc_master.opening_balance,
-                acc_master.debit,
-                acc_master.credit,
-                acc_master.place,
-                acc_master.phone2,
-                acc_departments.department AS openingdepartment
-            FROM acc_master
-            LEFT JOIN acc_departments ON acc_master.openingdepartment = acc_departments.department_id
-            WHERE acc_master.super_code = 'DEBTO'
+            SELECT
+                acc_ledgers.code,
+                acc_ledgers.particulars,
+                acc_ledgers.debit,
+                acc_ledgers.credit,
+                acc_ledgers.entry_mode,
+                acc_ledgers."date" AS entry_date,
+                CAST(acc_ledgers.voucher_no AS INTEGER) AS voucher_no,
+                acc_ledgers.narration
+            FROM acc_ledgers
+            LEFT JOIN acc_master ON acc_ledgers.code = acc_master.code
+            WHERE acc_master.super_code = 'DEBTO';
         """
     },
     "RRC_CLIENTS": {
